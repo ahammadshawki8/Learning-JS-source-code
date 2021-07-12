fb76db33d2c1041b0d1d8eb3540c2d816f9a9e2a
Ahammad Shawki <ahammadshawki8@gmail.com>
2021-07-12 08:05:21 +0600

    Some Projects Done

--- #PracticalJS/#projects/todo-list/script.js
+++ #PracticalJS/#projects/todo-list/script.js
@@ -2,6 +2,7 @@ let mainForm = document.querySelector("form");
 let taskField = document.querySelector("#task");
 let submitBtn = document.querySelector("button[type = 'submit']");
 let taskList = document.querySelector("#taskList");
+let allTask = [];
 
 submitBtn.disabled = true;
 
@@ -18,9 +19,32 @@ taskField.addEventListener("change", () => {
 
 mainForm.onsubmit = () => {
     let newTask = taskField.value;
-    taskList.innerHTML += `<li><a href = "#">${newTask}</a></li>`;
-
+    allTask.push({main: newTask, done: false});
+    renderTask();
     taskField.value = "";
     submitBtn.disabled = true;
     return false;
+}
+
+taskList.addEventListener("click", (e) => {
+    if (e.target.tagName = "LI"){
+        for (item of allTask) {
+            if (item.main === e.target.innerHTML) {
+                item.done = !(item.done);
+                renderTask();
+            }
+        }
+    }
+})
+
+
+function renderTask() {
+    taskList.innerHTML = "";
+    for (task of allTask) {
+        if (task.done === false) {
+            taskList.innerHTML += `<li><button>${task.main}</button></li>`
+        } else {
+            taskList.innerHTML += `<li><button><span style="text-decoration: line-through;">${task.main}</span></button></li>`
+        }
+}
 }
